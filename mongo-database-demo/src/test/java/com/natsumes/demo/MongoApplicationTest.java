a0c7bfa3a73e013451a68ca5f279f40b1400c0f9
SpikeLavender <hetengjiao_xjtu@163.com>
2020-12-06 20:54:44 +0800

    add redis module

--- mongo-database-demo/src/test/java/com/natsumes/demo/MongoApplicationTest.java
+++ mongo-database-demo/src/test/java/com/natsumes/demo/MongoApplicationTest.java
@@ -1,7 +1,6 @@
 package com.natsumes.demo;
 
 
-import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,7 +8,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
-@Slf4j
 public class MongoApplicationTest {
 
     @Test
---
+++ redis-database-demo/src/main/java/com/natsumes/redis/RedisApplication.java
@@ -0,0 +1,14 @@
+package com.natsumes.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hetengjiao
+ */
+@SpringBootApplication
+public class RedisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RedisApplication.class, args);
+    }
+}
